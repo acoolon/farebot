b69c3cb5927e86ce10bd6481f71da30533776782
acoolon <acoolon@web.de>
2013-06-27 12:11:18 +0200

    MifareDesfire: Protocol: Added Error Codes and ability

--- src/main/java/com/codebutler/farebot/card/desfire/DesfireProtocol.java
+++ src/main/java/com/codebutler/farebot/card/desfire/DesfireProtocol.java
@@ -34,14 +34,34 @@ public class DesfireProtocol {
     static final byte GET_ADDITIONAL_FRAME      = (byte) 0xAF;
     static final byte SELECT_APPLICATION        = (byte) 0x5A;
     static final byte READ_DATA                 = (byte) 0xBD;
+    static final byte GET_VALUE                 = (byte) 0x6C;
     static final byte READ_RECORD               = (byte) 0xBB;
     static final byte GET_FILES                 = (byte) 0x6F;
     static final byte GET_FILE_SETTINGS         = (byte) 0xF5;
 
     /* Status codes */
-    static final byte OPERATION_OK      = (byte) 0x00;
-    static final byte PERMISSION_DENIED = (byte) 0x9D;
-    static final byte ADDITIONAL_FRAME  = (byte) 0xAF;
+    static final byte OPERATION_OK          = (byte) 0x00;
+    static final byte NO_CHANGES            = (byte) 0x0C;
+    static final byte OUT_OF_EEPROM_ERROR   = (byte) 0x0E;
+    static final byte ILLEGAL_COMMAND_CODE  = (byte) 0x1C;
+    static final byte INTEGRITY_ERROR       = (byte) 0x1E;
+    static final byte NO_SUCH_KEY           = (byte) 0x40;
+    static final byte LENGTH_ERROR          = (byte) 0x7E;
+    static final byte PERMISSION_DENIED     = (byte) 0x9D;
+    static final byte PARAMETER_ERROR       = (byte) 0x9E;
+    static final byte APPICATION_NOT_FOUND  = (byte) 0xA0;
+    static final byte APPL_INTEGRITY_ERROR  = (byte) 0xA1;
+    static final byte AUTHENTICATION_ERROR  = (byte) 0xAE;
+    static final byte ADDITIONAL_FRAME      = (byte) 0xAF;
+    static final byte BOUNDARY_ERROR        = (byte) 0xBE;
+    static final byte PICC_INTEGRITY_ERROR  = (byte) 0xC1;
+    static final byte COMMAND_ABORTED       = (byte) 0xCA;
+    static final byte PICC_DISABLED_ERROR   = (byte) 0xCD;
+    static final byte COUNT_ERROR           = (byte) 0xCE;
+    static final byte DUPLICATE_ERROR       = (byte) 0xDE;
+    static final byte EEPROM_ERROR          = (byte) 0xEE;
+    static final byte FILE_NOT_FOUND        = (byte) 0xF0;
+    static final byte FILE_INTEGRITY_ERROR  = (byte) 0xF1;
 
     private IsoDep mTagTech;
 
@@ -104,6 +124,10 @@ public class DesfireProtocol {
         });
     }
 
+    public byte[] getValue (int fileNo) throws Exception {
+        return sendRequest(GET_VALUE, new byte[] {(byte) fileNo});
+    }
+
     public byte[] readRecord (int fileNum) throws Exception {
         return sendRequest(READ_RECORD, new byte[]{
                 (byte) fileNum,
@@ -128,17 +152,53 @@ public class DesfireProtocol {
             output.write(recvBuffer, 0, recvBuffer.length - 2);
 
             byte status = recvBuffer[recvBuffer.length - 1];
-            if (status == OPERATION_OK) {
+            if (status == OPERATION_OK)
                 break;
-            } else if (status == ADDITIONAL_FRAME) {
-                recvBuffer = mTagTech.transceive(wrapMessage(GET_ADDITIONAL_FRAME, null));
-            } else if (status == PERMISSION_DENIED) {
+            else if (status == NO_CHANGES)
+                throw new Exception("No changes done. Transaction not necessary.");
+            else if (status == OUT_OF_EEPROM_ERROR)
+                throw new Exception("Insufficient NV-Memory to complete command");
+            else if (status == ILLEGAL_COMMAND_CODE)
+                throw new Exception("Command code not supported");
+            else if (status == INTEGRITY_ERROR)
+                throw new Exception("Padding bytes not valid");
+            else if (status == NO_SUCH_KEY)
+                throw new Exception("Invalid key number");
+            else if (status == LENGTH_ERROR)
+                throw new Exception("Lenght of command string invalid");
+            else if (status == PERMISSION_DENIED)
                 throw new Exception("Permission denied");
-            } else {
+            else if (status == PARAMETER_ERROR)
+                throw new Exception("Values of parameters invalid");
+            else if (status == APPICATION_NOT_FOUND)
+                throw new Exception("Requested AID not present on PICC");
+            else if (status == APPL_INTEGRITY_ERROR)
+                throw new Exception("Unrecoverable error within application");
+            else if (status == AUTHENTICATION_ERROR)
+                throw new Exception("Authentication Error");
+            else if (status == ADDITIONAL_FRAME)
+                recvBuffer = mTagTech.transceive(wrapMessage(GET_ADDITIONAL_FRAME, null));
+            else if (status == BOUNDARY_ERROR)
+                throw new Exception("Attempt to read/write data from/to beyond the file's/record's limits");
+            else if (status == PICC_INTEGRITY_ERROR)
+                throw new Exception("Unrecoverable error within PICC");
+            else if (status == COMMAND_ABORTED)
+                throw new Exception("Command aborted");
+            else if (status == PICC_DISABLED_ERROR)
+                throw new Exception("PICC was disabled by an unrecoverable error");
+            else if (status == COUNT_ERROR)
+                throw new Exception("Number of Applications limited to 28");
+            else if (status == DUPLICATE_ERROR)
+                throw new Exception("File/Application already exists");
+            else if (status == EEPROM_ERROR)
+                throw new Exception("EEPROM error due to loss of power");
+            else if (status == FILE_NOT_FOUND)
+                throw new Exception("File not found");
+            else if (status == FILE_INTEGRITY_ERROR)
+                throw new Exception("Unrecoverable error within file");
+            else
                 throw new Exception("Unknown status code: " + Integer.toHexString(status & 0xFF));
-            }
         }
-        
         return output.toByteArray();
     }
 
